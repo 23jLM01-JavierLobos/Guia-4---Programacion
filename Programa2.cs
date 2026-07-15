c5ac9ce948dd8c2c4d760e13c9241a6c442818df
23jLM01-JavierLobos <lobos.ale.15@gmail.com>
2026-07-15 09:05:11 -0600

    Add files via upload

---
+++ Programa2.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nombre: Javier Lobos\n" + "Grado: IV bach - clave 17\n" + "====Calculadora de Cambio===");
+        int pago;
+        string producto;
+        Console.Write("Digite el nombre producto:");
+        producto = Console.ReadLine();
+        Console.Write("Dame el precio del producto: ");
+        int precio = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite el valor del billete: ");
+        pago = Convert.ToInt32(Console.ReadLine());
+        int cambio = pago - precio;
+        Console.Write("Su cambio es: " + cambio);
+
+    }
+}
