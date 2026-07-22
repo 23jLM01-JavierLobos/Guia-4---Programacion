5e643bd245f3421a48b5ae956a7a09a5610db2fb
23jLM01-JavierLobos <lobos.ale.15@gmail.com>
2026-07-21 18:52:38 -0600

    Create Lecc34_Act1_Inc3.cs

---
+++ Lecc34_Act1_Inc3.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Nombre: Javier Lobos\n" + "Grado: IV bach - clave 17\n" + "====Grados===");  
+      double c, f, k;
+
+        Console.Write("Ingrese la cantidad en grados centigrados: ");
+        c = Convert.ToDouble(Console.ReadLine());
+
+        f = (c * 1.8) + 32;
+        k = c + 273.15;
+
+        Console.WriteLine($"La cantidad de grados celsius es de: {c}");
+        Console.WriteLine($"La cantidad de grados farhenheit es de: {f}");
+        Console.WriteLine($"La cantidad de grados kelvin es de: {k}");
+    }
+}
