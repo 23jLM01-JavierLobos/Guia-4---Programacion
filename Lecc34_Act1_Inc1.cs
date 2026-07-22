f915af898ce020658921e6ba3ac1605e693eacb6
23jLM01-JavierLobos <lobos.ale.15@gmail.com>
2026-07-21 18:49:17 -0600

    Create Lecc34_Act1_Inc1.cs

---
+++ Lecc34_Act1_Inc1.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nombre: Javier Lobos\n" + "Grado: IV bach - clave 17\n" + "====Retos===");
+        int nd, r;
+        string b;
+
+        Console.Write("Digite un numero decimal: ");
+        nd = Convert.ToInt32(Console.ReadLine());
+
+        b = "";
+
+        if (nd == 0)
+        {
+            b = "0";
+        }
+        else
+        {
+            while (nd > 0)
+            {
+                r = nd % 2; 
+
+                b = r.ToString() + b;
+
+ 
+                nd = nd / 2;
+            }
+        }
+
+        Console.WriteLine($"El numero en binario es: {b}");
+    }
+}
